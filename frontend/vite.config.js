6e3242a76da2b0a927c21bfa5e63f08b86dc6d07
Sunilkumarsinghj <sunilworld1234ss@gmail.com>
2025-05-20 22:54:48 +0530

    completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 export default defineConfig({
-  server:{
-    host:"0.0.0.0",
-    fs:{
-      strict:false,
-    }
-  },
-  plugins: [react(),],
+  plugins: [react()],
 })
